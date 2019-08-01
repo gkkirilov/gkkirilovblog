4dbeff6fb73c9423077b91ce0430cbbbb586bf3b
Georgi Kirilov <gkkirilov@gmail.com>
2019-08-01 11:51:32 +0300

    Add youtube link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,44 @@
 module.exports = {
-  title: '@gkkirilov blog',
-  description: 'Just playing around! 🚀',
+  title: "@gkkirilov blog",
+  description: "Just playing around! 🚀",
   head: [
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
+        rel: "icon",
         href:
-          'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/153/shocked-face-with-exploding-head_1f92f.png'
+          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/153/shocked-face-with-exploding-head_1f92f.png"
       }
     ]
   ],
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Twitter', link: 'https://twitter.com/gkkirilov' },
-      { text: 'GitHub', link: 'https://github.com/gkkirilov' },
-      { text: 'About', link: '/about' }
+      { text: "Home", link: "/" },
+      { text: "Twitter", link: "https://twitter.com/gkkirilov" },
+      { text: "GitHub", link: "https://github.com/gkkirilov" },
+      {
+        text: "YouTube",
+        link: "https://youtube.com/channel/UCyxPZRn94PPwDTsfejtqX2w"
+      },
+      { text: "About", link: "/about" }
     ]
   },
 
   plugins: {
-    '@vuepress/search': {
-      test: '/posts/'
+    "@vuepress/search": {
+      test: "/posts/"
     },
-    '@vuepress/pwa': true,
-    '@vuepress/plugin-nprogress': true,
-    '@vuepress/plugin-last-updated': true,
+    "@vuepress/pwa": true,
+    "@vuepress/plugin-nprogress": true,
+    "@vuepress/plugin-last-updated": true,
 
-    '@vuepress/google-analytics': {
-      ga: '' // UA-00000000-0
+    "@vuepress/google-analytics": {
+      ga: "" // UA-00000000-0
     },
-    '@vuepress/blog': true,
-    '@vuepress/back-to-top': true,
+    "@vuepress/blog": true,
+    "@vuepress/back-to-top": true,
     sitemap: {
-      hostname: 'https://gkkirilov.com'
+      hostname: "https://gkkirilov.com"
     },
     seo: {
       /* options */
